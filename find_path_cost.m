f377f3a988f8d97b705013dcf45163be61b36a2b
Robert <rpalfini@uci.edu>
2023-03-09 13:07:14 -0800

    fixed path cost calculation

--- find_path_cost.m
+++ find_path_cost.m
@@ -1,6 +1,12 @@
 function cost = find_path_cost(points)
 % points is an array formatted [point1; point2;...;pointN] that represents
 % the points of our solution
-distances = pdist(points);
+
+% calculate distances between points
+n = size(points, 1);
+distances = zeros(n,1);
+for i = 1:n-1
+    distances(i) = norm(points(i,:) - points(i+1,:));
+end
 cost = sum(distances);
 end
